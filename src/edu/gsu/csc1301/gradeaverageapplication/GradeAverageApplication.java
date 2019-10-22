698023837300f7a67f893fafcbd666d119c54bad
Ariana Tyson <arianatyson@live.com>
2019-10-22 00:30:44 -0400

    Created program and added header comment.

--- src/edu/gsu/csc1301/gradeaverageapplication/GradeAverageApplication.java
+++ src/edu/gsu/csc1301/gradeaverageapplication/GradeAverageApplication.java
@@ -1,5 +1,15 @@
 package edu.gsu.csc1301.gradeaverageapplication;
 
+/**
+ * This creates a grade average for students by calculating their average overall percentage
+ * and letter grade, from previous test scores.
+ * 
+ * 
+ * @author Ariana Tyson
+ * @version 10/21/2019
+ *
+ */
+
 public class GradeAverageApplication {
 
 	public static void main(String[] args) {
