e6753319c6b97997ed591c8c8826487bde4a6307
Ariana Tyson <arianatyson@live.com>
2019-10-22 00:36:09 -0400

    Renaming source file and setting up layout for program.

--- src/edu/gsu/csc1301/gradeaverageapplication/GradeAverage.java
+++ src/edu/gsu/csc1301/gradeaverageapplication/GradeAverage.java
@@ -10,10 +10,17 @@ package edu.gsu.csc1301.gradeaverageapplication;
  *
  */
 
-public class GradeAverageApplication {
+public class GradeAverage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
+		
+		//Input
+		
+		//Computation
+		
+		//Output
 
 	}
 
