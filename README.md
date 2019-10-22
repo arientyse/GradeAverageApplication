fd2c3ef4ebfa9de1baa8ab09e49179ad514d3cc3
Ariana Tyson <arianatyson@live.com>
2019-10-22 00:30:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GradeAverageApplication
+ Project Two from CSC 1301. Calculates a Grade Average from previous test score values.
---
+++ src/edu/gsu/csc1301/gradeaverageapplication/GradeAverageApplication.java
@@ -0,0 +1,10 @@
+package edu.gsu.csc1301.gradeaverageapplication;
+
+public class GradeAverageApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
